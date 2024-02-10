eebeffde7abd2f0d14020f57a6f38bca9cd8a11b
Danilo Rodrigues <mydanilows@gmail.com>
2024-02-10 09:35:22 -0300

    Adiciona dependência @swagger-api/apidom-ns-asyncapi-2 e atualiza arquivo react-swagger.tsx

--- src/app/api-doc/react-swagger.tsx
+++ src/app/api-doc/react-swagger.tsx
@@ -1,14 +1,10 @@
-'use client';
-
+import React from 'react';
 import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
-
+import 'swagger-ui-react/swagger-ui.css';// Caminho para o seu arquivo Swagger
 type Props = {
   spec: Record<string, any>,
 };
 
-function ReactSwagger({ spec }: Props) {
-  return <SwaggerUI spec={spec} />;
-}
+const Swagger = ({ spec }: Props) => <SwaggerUI spec={spec} />;
 
-export default ReactSwagger;
+export default Swagger;
